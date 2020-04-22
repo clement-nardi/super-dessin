319f7c33329162c51b84e57c8a05dcda6e24ffd6
Clément Nardi <clementnardi@gmail.com>
2020-04-22 21:24:04 +0200

    multi-triangle hexagongit status

--- src/components/SuperDessin.vue
+++ src/components/SuperDessin.vue
@@ -7,49 +7,71 @@
 </template>
 
 <script>
-import {Point,Circle,Segment} from '@flatten-js/core';
+import {point,segment,line,vector} from '@flatten-js/core';
 
 export default {
-  name: 'HelloWorld',
+  name: 'SuperDessin',
   props: {
     width: Number,
     height: Number
   },
   methods: {
     randomPoint: function () {
-      return new Point(  Math.floor(Math.random()*this.width),
+      return point(  Math.floor(Math.random()*this.width),
         Math.floor(Math.random()*this.height))
     },
     printTriangle: function (tr) {
       var svg = document.getElementById("mySvg")
-      svg.innerHTML += new Segment(tr[0],tr[1]).svg()
-      svg.innerHTML += new Segment(tr[1],tr[2]).svg()
-      svg.innerHTML += new Segment(tr[2],tr[0]).svg()
+      svg.innerHTML += segment(tr[0],tr[1]).svg()
+      svg.innerHTML += segment(tr[1],tr[2]).svg()
+      svg.innerHTML += segment(tr[2],tr[0]).svg()
     },
-    fillTriangle: function (tr) {
+    segmentToLine: function (s) {
+      return line(s.ps, s.pe)
+    },
+    fillTriangle: function (tr_, clockwise) {
+      let tr = tr_
+      if (!clockwise) {
+        tr = [tr_[0], tr_[2], tr_[1]]
+      }
       var svg = document.getElementById("mySvg")
-      let s1 = new Segment(tr[0],tr[1])
-      let s1r = s1.rotate(0.12, tr[0])
-      svg.innerHTML += s1r.svg()
+      let segments = [segment(tr[0],tr[1]), segment(tr[1],tr[2]), segment(tr[2],tr[0])]
+      while (segments[0].length > 3) {
+        let angle = (clockwise?1:-1) * 0.10
+        let s1r = segments[0].rotate(angle, segments[0].ps)
+        let destPoint = this.segmentToLine(s1r).intersect(this.segmentToLine(segments[1]))[0]
+        let s = segment(segments[0].ps, destPoint)
+        svg.innerHTML += s.svg()
+        segments = [segment(destPoint, segments[1].pe), segments[2], s]
+      }
     }
   },
   mounted: function () {
-    var svg = document.getElementById("mySvg")
 
-    // make some construction
-    let c1 = new Circle(new Point(200, 110), 50)
-    let s1 = new Segment(10, 10, 200, 200)
+      let center = point(this.width/2, this.height/2)
+      let centerRight = point(this.width*3/4, this.height/2)
+      let seg1 = segment(center, centerRight)
+      let seg2 = seg1.rotate(Math.PI/3, center)
 
-    svg.innerHTML += c1.svg()
-    svg.innerHTML += s1.svg()
+      let triangle = [center, centerRight, seg2.pe]
+      for (let i = 0; i < 6; i++) {
+        this.printTriangle(triangle)
+        this.fillTriangle(triangle, i%2)
 
+        let tr2 = triangle.map(p => p.translate(vector(triangle[0], triangle[1])))
+        this.printTriangle(tr2)
+        this.fillTriangle(tr2, i%2)
 
-    for (let i = 0; i < 3; i++) {
-      let tr = [this.randomPoint(), this.randomPoint(), this.randomPoint()]
-      this.printTriangle(tr)
-      this.fillTriangle(tr)
-    }
+        let tr3 = tr2.map(p => p.rotate(Math.PI/3, tr2[0]))
+        this.printTriangle(tr3)
+        this.fillTriangle(tr3, (i+1)%2)
 
+        let tr4 = tr2.map(p => p.rotate(-Math.PI/3, tr2[0]))
+        this.printTriangle(tr4)
+        this.fillTriangle(tr4, (i+1)%2)
+
+        triangle = triangle.map(p => p.rotate(Math.PI/3,center))
+      }
 
   }
 
